de51e443538ec197f8c668e93c2ce3176a3fcd88
David Valverde Ruiz <89070011+dvalverderuiz@users.noreply.github.com>
2025-02-25 15:44:29 +0100

    fix fecha

--- restore_pruebas.sh
+++ restore_pruebas.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # Parámetros
-FECHA=$1
+FECHA_USUARIO=$1
 RUTA_GUARDADO=$2
 
 # Repositorio backup remoto
 USUARIO_REMOTO="admin_backup"
-IP_REMOTA="100.77.20.50"
+IP_REMOTA="100.77.20.47"
 DIR_REMOTO="/dir_${USER}_backup"
+
+# Convertir la fecha de usuario (2025-02-24) al formato del archivo (20250224)
+FECHA_ARCHIVO=$(echo "$FECHA_USUARIO" | tr -d '-')
 
 # Verifica si el directorio remoto existe
 if ! ssh "${USUARIO_REMOTO}@${IP_REMOTA}" "[ -d '$DIR_REMOTO' ]"; then
@@ -15,11 +18,11 @@ if ! ssh "${USUARIO_REMOTO}@${IP_REMOTA}" "[ -d '$DIR_REMOTO' ]"; then
     exit 1
 fi
 
-# Busca el archivo en el directorio remoto basado en la fecha
-ARCHIVO_REMOTO=$(ssh "${USUARIO_REMOTO}@${IP_REMOTA}" "find '$DIR_REMOTO' -name '*${FECHA}*' -type f")
+# Busca el archivo en el directorio remoto basado en la fecha convertida
+ARCHIVO_REMOTO=$(ssh "${USUARIO_REMOTO}@${IP_REMOTA}" "find '$DIR_REMOTO' -name '*${FECHA_ARCHIVO}*' -type f")
 
 if [ -z "$ARCHIVO_REMOTO" ]; then
-    echo "No se encontró ningún archivo con la fecha $FECHA en $DIR_REMOTO."
+    echo "No se encontró ningún archivo con la fecha $FECHA_USUARIO en $DIR_REMOTO."
     exit 1
 fi
 
