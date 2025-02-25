a324a935e1e3e2bb925b7d2951a0728fe1e3f359
David Valverde Ruiz <89070011+dvalverderuiz@users.noreply.github.com>
2025-02-25 13:28:39 +0100

    add archivo restore_pruebas.sh con script actualizado para nuestra version

---
+++ restore_pruebas.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Parámetros
+ARCHIVO=$1
+RUTA_GUARDADO=$2
+
+
+# Repositorio backup remoto
+USUARIO_REMOTO="admin_backup"  
+IP_REMOTA="100.77.20.47"
+DIR_REMOTO="/dir_${USER}_backup"
+
+
